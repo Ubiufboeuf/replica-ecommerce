a193c00fe865acecc46a538182bfdb6a122a50f0
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-30 13:22:08 -0300

    chore: :rotating_light: corregir sugerencias de eslint

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,12 @@
-import type { Route } from "./+types/home";
+import type { Route } from './+types/home'
 
-export function meta({}: Route.MetaArgs) {
+export function meta ({}: Route.MetaArgs) {
   return [
-    { title: "Réplica e-commerce" },
-    { name: "description", content: "Réplica de un e-commerce como última tarea de programación" },
-  ];
+    { title: 'Réplica e-commerce' },
+    { name: 'description', content: 'Réplica de un e-commerce como última tarea de programación' }
+  ]
 }
 
-export default function Home() {
-  return '';
+export default function Home () {
+  return ''
 }
