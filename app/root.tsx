d957e12b010b96a9f7f970d7eb558e796b22936a
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-30 13:38:24 -0300

    feat: renombrar icono y añadir logo

--- app/root.tsx
+++ app/root.tsx
@@ -26,7 +26,7 @@ export function Layout ({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='shortcut icon' href='/logo_walmart.webp' />
+        <link rel='shortcut icon' href='/icon.webp' />
         <Meta />
         <Links />
       </head>
